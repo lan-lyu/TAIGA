78011e83d0e1dd1162da03d2b0b16d090faff4c3
Alyssa14411 <pivrpiv@126.com>
2023-06-20 05:45:20 -0700

    add google switch

--- js/switchSearchLayout.js
+++ js/switchSearchLayout.js
@@ -11,3 +11,17 @@ function toggleLayoutBack() {
     window.location.href = "search.html";
   }
 }
+
+function toogleGoogle(){
+  var checkBox = document.getElementById("switchLayout-google-2");
+  if (checkBox.checked == true){
+    window.location.href = "search-2-google.html";
+  }
+}
+
+function toogleSearch2(){
+  var checkBox = document.getElementById("switchLayout-google");
+  if (checkBox.checked == false){
+    window.location.href = "search-2.html";
+  }
+}
